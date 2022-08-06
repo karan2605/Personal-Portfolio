caf3555ae2c1c1f113c656a7a85b945548fc674a
Karan Patel <karan2605@hotmail.co.uk>
2022-08-06 13:42:45 +0100

    text size made larger, changed contact button style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,22 +7,25 @@ export default function Navbar() {
     <header className="bg-gray-800 md:sticky top-0 z-10">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <div className="title-font font-medium text-white mb-4 md:mb-0">
-          <a href="#about" className="ml-3 text-xl">
+          <a href="#about" className="ml-3 text-2xl">
             Karan Patel
           </a>
         </div>
         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
-          <a href="#projects" className="mr-5 hover:text-white">
+          <a href="#MoreAbout" className="text-xl mr-5 hover:text-white">
+            A Bit More About Me
+          </a>
+          <a href="#projects" className="text-xl mr-5 hover:text-white">
             Past Work
           </a>
-          <a href="#skills" className="mr-5 hover:text-white">
+          <a href="#skills" className="text-xl mr-5 hover:text-white">
             Skills
           </a>
         </nav>
         <a
           href="#contact"
-          className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
-          Hire Me
+          className="inline-flex items-center bg-red-600 text-white border-0 py-1 px-3 focus:outline-none hover:bg-red-700 rounded text-xl mt-4 md:mt-0">
+          Contact Me
           <ArrowRightIcon className="w-4 h-4 ml-1" />
         </a>
       </div>
