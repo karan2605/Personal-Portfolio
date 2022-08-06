8ae69f007fc30515a49a481f94cadd440293345f
Karan Patel <karan2605@hotmail.co.uk>
2022-08-06 13:41:58 +0100

    text and font adjustment

--- src/components/About.js
+++ src/components/About.js
@@ -1,25 +1,28 @@
 import React from "react";
+import { ReactComponent as Logo } from '../coding-svg-clipart.svg';
 
 export default function About() {
   return (
     <section id="about">
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+          <h1 className="title-font lg:text-6xl text-3xl mb-4 font-medium text-white">
             Hi, I'm Karan.
-            <br className="hidden lg:inline-block" />I love to build amazing
-            apps.
+            <br className="hidden lg:inline-block" />I am a Software Engineer.
           </h1>
-          <p className="mb-8 leading-relaxed">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
-            laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
-            Laborum, voluptas natus?
+          <p className="text-xl mb-8 leading-relaxed">
+            I enjoy using technology and programming to solve problems and build innovative software.
           </p>
           <div className="flex justify-center">
             <a
               href="#contact"
               className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
               Work With Me
+            </a>
+            <a
+              href="#MoreAbout"
+              className="ml-4 inline-flex text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded text-lg">
+              A bit more about me
             </a>
             <a
               href="#projects"
@@ -29,11 +32,8 @@ export default function About() {
           </div>
         </div>
         <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-          <img
-            className="object-cover object-center rounded"
-            alt="hero"
-            src="./coding.svg"
-          />
+          <Logo/>
+          
         </div>
       </div>
     </section>
