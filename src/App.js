53d0aeb813a8fd0cf326d775f9c7b95bbd749d88
Karan Patel <karan2605@hotmail.co.uk>
2022-08-06 13:41:20 +0100

    added more about page

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import About from './components/About';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
 import Contact from './components/Contact';
+import MoreAbout from './components/MoreAbout'
 
 export default function App() {
   return (
-    
     <main className="text-gray-400 bg-gray-900 body-font">
       <Navbar />
       <About />
+      <MoreAbout />
       <Projects />
       <Skills />
       <Contact />
