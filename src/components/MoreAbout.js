8ce5bbbeb8753ee61ab10e07118a5b7ca5a805db
Karan Patel <karan2605@hotmail.co.uk>
2022-08-07 09:34:51 +0100

    Added content

--- src/components/MoreAbout.js
+++ src/components/MoreAbout.js
@@ -10,10 +10,19 @@ export default function MoreAbout() {
             <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-4">
                 A Bit More About Me
             </h1>
-            <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi sit
-                ipsa delectus eum quo voluptas aspernatur accusantium distinctio
-                possimus est.
+            <p className="text-xl leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
+                A recent Computer Science graduate from the University of Warwick, embarking
+                on the new journey of work as a Software Engineer at HSBC. I'm naturally a keen 
+                problem solver who enjoys being challenged to always search for more innovative 
+                and efficient approaches.<br></br><br></br>
+
+                I have great interest in Blockchain technology and wish to persue a career as a 
+                Blockchain developer in the future. My interest in this began while researching
+                and developing my final year project at university.<br></br><br></br>
+
+                In my free time I enjoy weightlifting in the gym and watching sports. Specifically, watching 
+                football, cricket and Formula One. Recently, I have also delved into Leetcode programming to
+                improve my problem solving skills.
             </p>
             </div>
           </div>
