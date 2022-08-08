c757295dc5e07c4eb0e570ff600d2e5dab06a5a5
Karan Patel <karan2605@hotmail.co.uk>
2022-08-08 20:18:46 +0100

    Added my projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,25 +11,19 @@ export default function Projects() {
           <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
             My Projects
           </h1>
-          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo
-            facilis repellat ab cupiditate alias vero aliquid obcaecati quisquam
-            fuga dolore.
+          <p className="lg:w-2/3 mx-auto leading-relaxed text-xl">
+            Listed below are all the projects I have worked on over the past few years 
+            at university and outside.
           </p>
         </div>
         <div className="flex flex-wrap -m-4">
           {projects.map((project) => (
             <a
               href={project.link}
-              key={project.image}
+              key={project.key}
               className="sm:w-1/2 w-100 p-4">
               <div className="flex relative">
-                <img
-                  alt="gallery"
-                  className="absolute inset-0 w-full h-full object-cover object-center"
-                  src={project.image}
-                />
-                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900  hover:opacity-100">
                   <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
                     {project.subtitle}
                   </h2>
