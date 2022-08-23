7cf9acd66d062da6e886763ae6622998994f0bc9
Karan Patel <karan2605@hotmail.co.uk>
2022-08-23 15:25:21 +0100

    Built blog post component, renders and styles md files

---
+++ src/components/BlogPost.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import ReactMarkdown from 'markdown-to-jsx';
+
+
+export default function BlogPost(props) {
+    const [blogPost, setPost] = useState('');
+    const { open, blog } = props;
+
+    useEffect(() => {
+        import(`../content/blog/${blog}/index.md`)
+            .then(res => {
+                fetch(res.default)
+                    .then(res => res.text())
+                    .then(res => setPost(res))
+                    .catch(err => console.log(err));
+            })
+            .catch(err => console.log(err));
+    });
+    
+    return(
+       <> 
+       {open && <ReactMarkdown className="prose-zinc lg:prose-xl prose-img:rounded-xl prose-headings:bold 
+       prose-a:text-blue-600 prose-h2:font-semibold prose-h1:font-semibold prose-h3:italic prose-blockquote:italic" children={blogPost} />} </>
+    )
+}
