6e6aaf4f1d15c174869f5f4a5fc00029d67b1e6d
Karan Patel <karan2605@hotmail.co.uk>
2022-08-23 15:24:02 +0100

    Added Blog component and react router component structure

--- src/App.js
+++ src/App.js
@@ -4,17 +4,25 @@ import About from './components/About';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
 import Contact from './components/Contact';
-import MoreAbout from './components/MoreAbout'
+import MoreAbout from './components/MoreAbout';
+import Blog from './components/Blog';
+
+import {
+  BrowserRouter,
+} from "react-router-dom";
 
 export default function App() {
   return (
     <main className="text-gray-400 bg-gray-900 body-font">
-      <Navbar />
-      <About />
-      <MoreAbout />
-      <Projects />
-      <Skills />
-      <Contact />
+      <BrowserRouter>
+        <Navbar />
+        <About />
+        <MoreAbout />
+        <Projects />
+        <Skills />
+        <Blog />
+        <Contact />
+      </BrowserRouter>
     </main>
   );
 }
