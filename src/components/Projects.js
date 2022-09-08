4bc65bc382132e35d9f5bee40bc44a9357b4027c
Karan Patel <karan2605@hotmail.co.uk>
2022-09-08 20:56:17 +0100

    shows projects on card click

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,16 @@
 import { CodeIcon } from "@heroicons/react/solid";
-import React from "react";
+import React, { useState } from 'react';
 import { projects } from "../data";
 
 export default function Projects() {
+  const [show, setShow] = useState(-1);
+
+  const onClickHandle = (i) => {
+    
+    setShow(show === i ? -1 : i);
+  
+  };
+
   return (
     <section id="projects" className="text-gray-400 bg-gray-900 body-font">
       <div className="container px-5 py-10 mx-auto text-center lg:px-40">
@@ -17,10 +25,11 @@ export default function Projects() {
           </p>
         </div>
         <div className="flex flex-wrap -m-4">
-          {projects.map((project) => (
+          {projects.map((project, i) => (
             <a
               href={project.link}
               key={project.key}
+              onClick={() => onClickHandle(i)}
               className="sm:w-1/2 w-100 p-4">
               <div className="flex relative">
                 <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900  hover:opacity-100">
@@ -30,7 +39,7 @@ export default function Projects() {
                   <h1 className="title-font text-lg font-medium text-white mb-3">
                     {project.title}
                   </h1>
-                  <p className="leading-relaxed">{project.description}</p>
+                   <p className="leading-relaxed">{show === i && project.description}</p>
                 </div>
               </div>
             </a>
